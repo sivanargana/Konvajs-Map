d0e27427274d39161da7b53b59a714272f417fb6
sivanargana <siva.nargana66@gmail.com>
2024-09-20 22:49:38 +0530

    new changes

--- map/index.js
+++ map/index.js
@@ -1,22 +1,13 @@
-import './style.css';
+import './map.scss';
 import Map from './map.webp';
 import * as Konva from 'konva';
 import Resizing from './resize';
 import Panning from './panning';
 import Zooming from './zooming';
-
- 
-
-
-
 let stage = new Konva.Stage({
   container: 'canvas',
-
 });
-
 let layer = new Konva.Layer();
-
-
 let rect = new Konva.Rect({
   x: 300,
   y: 600,
@@ -26,38 +17,16 @@ let rect = new Konva.Rect({
   stroke: 'black',
   strokeWidth: 4,
 });
-
-
 Konva.Image.fromURL(Map, function (image) {
-
-
-
   image.setAttrs({
     x: 0,
     y: 0,
-
   });
- 
   layer.add(image);
   layer.add(rect);
-
-
 });
-
-
-
-
-
-
-
 stage.add(layer);
 layer.draw();
-
-
-
-
-
-
 Resizing(stage)
-Panning(stage,layer)
-Zooming(stage,layer)
+Panning(stage, layer)
+Zooming(stage, layer)
